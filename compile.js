b0668ade9646dc48a45a8c2d73c9becb3e9c2b0e
Jing Conan Wang <hbhzwj@gmail.com>
2015-04-05 02:14:50 -0700

    revert to 268c331a9ebb307277be5d533e4a9d1bf51c953f

--- compile.js
+++ compile.js
@@ -5,37 +5,10 @@
 glob = require('glob');
 path = require('path');
 fs = require('fs');
-
-var scriptFilePaths = [
-    "scripts/namespace.js",
-    "scripts/util.js",
-    "scripts/script.js",
-    "scripts/menu.js"
-];
-
 function concat_template() {
     var filepath;
-    // var code = fs.readFileSync("script.js").toString();
-    var code = "";
-    // var scriptFilePaths = glob.sync("scripts/*.js");
-    for(var i = 0; i < scriptFilePaths.length; ++i) {
-        filepath = scriptFilePaths[i]
-        console.log("Compile script: " + filepath);
-        code += (fs.readFileSync(filepath).toString() + '\n\n');
-    }
-
-
+    var code = fs.readFileSync("script.js").toString();
     code += "\n\n";
-
-    // Compile frameworks
-    var frameworkFilePaths = glob.sync("frameworks/*.js");
-    for(var i = 0; i < frameworkFilePaths.length; ++i) {
-        filepath = frameworkFilePaths[i]
-        console.log("Compile framework: " + filepath);
-        code += (fs.readFileSync(filepath).toString() + '\n\n');
-    }
-
-    // Compile templates
     var templateFilepaths = glob.sync("templates/*.html");
     for(var i = 0; i < templateFilepaths.length; ++i) {
         filepath = templateFilepaths[i]
@@ -43,7 +16,14 @@ function concat_template() {
         key = path.basename(filepath, '.html');
         var templateCode = fs.readFileSync(filepath).toString();
         template = templateCode.replace(/\n/g, '').replace(/\"/g, '\'');
-        code += ("GTD.templates." + key + " = \"" + template + "\";\n\n");
+        code += ("app.templates." + key + " = \"" + template + "\";\n\n");
+    }
+
+    var frameworkFilePaths = glob.sync("frameworks/*.js");
+    for(var i = 0; i < frameworkFilePaths.length; ++i) {
+        filepath = frameworkFilePaths[i]
+        console.log("Compile framework: " + filepath);
+        code += (fs.readFileSync(filepath).toString() + '\n\n');
     }
 
     filepath = 'build/compiled_script.js';
--- scripts/namespace.js
+++
@@ -1,8 +0,0 @@
-var GTD = {
-  body: DocumentApp.getActiveDocument().getBody(),
-  header: ['Actionable', 'Waiting For', 'Done'], //FIXME change to taskStatus
-  headerColor: ['#ff0000', '#9d922e', '#16a031'], //FIXME change to taskStatusColor 
-  defaultRows: 1,
-  templates: {},
-  TOC: {},
-};
